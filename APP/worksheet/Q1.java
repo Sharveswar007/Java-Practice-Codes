32475d2e31c2b1a7cebf42c4f66726d9b1432c49
SHARVESWAR.M <sm9187@srmist.edu.in>
2025-08-20 09:40:11 +0530

    Update Q1.java

--- APP/worksheet/Q1.java
+++ APP/worksheet/Q1.java
@@ -1,7 +1,7 @@
 public class Q1 {
     void greet() {
         System.out.println("Hello, World!");
-    }
+    } 
     public static void main(String args[]){
         Q1 obj = new Q1();
         obj.greet();
